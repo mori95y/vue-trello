80217075ff499ce3cf6b14af7e6f7a4cac44e907
mori95y <mori95y@gmail.com>
2022-11-22 02:54:33 +0900

    Board, ListAddを追加

---
+++ src/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            vue Trello
+        </header>
+        <main>
+            <p class="info-line">All: 0 tasks</p>
+            <list-add />
+        </main>
+    </div>
+</template>
+
+<script>
+import ListAdd from './ListAdd.vue'
+export default {
+    components: {
+        ListAdd
+    },
+}
+</script>
